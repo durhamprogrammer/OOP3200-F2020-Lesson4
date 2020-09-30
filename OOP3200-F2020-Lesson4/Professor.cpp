c6c576fcdf884d0d4d825095f086c63cc6427bd6
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-09-30 11:07:08 -0400

    Add project files.

---
+++ OOP3200-F2020-Lesson4/Professor.cpp
@@ -0,0 +1,51 @@
+/**
+ * Project OOP3200-F2020-Lesson4
+ * @author Tom Tsiliopoulos
+ * @version 1.0
+ */
+
+
+#include "Professor.h"
+
+/**
+ * Professor implementation
+ */
+
+
+/**
+ * @param first_name
+ * @param last_name
+ * @param age
+ * @param employee_id
+ */
+void Professor::Professor(string first_name, string last_name, float age, string employee_id) {
+
+}
+
+/**
+ * @return string
+ */
+string Professor::getEmployeeID() {
+    return "";
+}
+
+/**
+ * @param value
+ */
+void Professor::setEmployeeID(string value) {
+
+}
+
+/**
+ * @return void
+ */
+void Professor::Teaches() {
+    return;
+}
+
+/**
+ * @return string
+ */
+string Professor::ToString() {
+    return "";
+}
