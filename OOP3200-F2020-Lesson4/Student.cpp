35468188318912f3d6343a363e8b859c3a62eb8e
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-09-30 11:35:09 -0400

    updated Student class

--- OOP3200-F2020-Lesson4/Student.cpp
+++ OOP3200-F2020-Lesson4/Student.cpp
@@ -7,6 +7,8 @@
 
 #include "Student.h"
 
+#include <iostream>
+
 /**
  * Student implementation
  */
@@ -18,34 +20,46 @@
  * @param age
  * @param student_id
  */
-void Student::Student(string first_name, string last_name, float age, string student_id) {
-
+Student::Student(const std::string& first_name, const std::string& last_name, const float age, std::string student_id)
+: Person(first_name, last_name, age), m_studentId(std::move(student_id))
+{
 }
 
 /**
- * @return string
+ * @return std::string
  */
-string Student::getStudentID() {
-    return "";
+std::string Student::getStudentID() const
+{
+    return m_studentID;
 }
 
 /**
  * @param value
  */
-void Student::setStudentID(string value) {
-
+void Student::setStudentID(const std::string& value)
+{
+    m_studentID = value;
 }
 
 /**
  * @return void
  */
-void Student::Studies() {
-    return;
+void Student::Studies() const
+{
+    std::cout << getFirstName() << " is studying" << std::endl;
 }
 
 /**
- * @return string
+ * @return std::string
  */
-string Student::ToString() {
-    return "";
+std::string Student::ToString()
+{
+    std::string output_string;
+
+    output_string += Person::ToString();
+    output_string += "-------------------------------------------\n";
+    output_string += "Student ID: " + getStudentID() + "\n";
+    output_string += "-------------------------------------------\n";
+	
+    return output_string;
 }
